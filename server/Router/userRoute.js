ce74b3534c11e0d50703bbdce36886bc56a40fa3
andrew <andrewjthomsen@yahoo.com>
2019-08-12 18:30:57 -0700

    Fixed routing issues in userRoute.

--- server/Router/userRoute.js
+++ server/Router/userRoute.js
@@ -1,48 +1,47 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../Models/User");
+const User = require('../models/User');
 
-// GET ALL USERS ROUTE
-router.get("/", (req, res) => {
-  User.find().then(users => {
-    res.status(200).json(users);
-  });
+// Route responsible for getting all users
+router.get('/', (req, res) => {
+    User.find().then(users => {
+            res.status(200).json(users);
+        });
 });
 
-// GET SINGLE USER -> Route works correctly
-router.get("/api/users/:id", (req, res) => {
-  User.findByID(req.params.id)
-    .then(user => {
-      user
-        ? res.status(200).json(user)
-        : res.status(404).json({ Error: `User with that id doesn't exist...` });
-    })
-    .catch(err => res.status(500).json({ Error: err }));
+// Route to get a single user by id
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+              } else {
+                res.status(404).send();
+              }
+        });
 });
 
-// CREATE NEW USER
-router.post("/", (req, res) => {
-  const newUser = new User(req.body);
-  newUser
-    .save()
-    .then(user => res.status(201).json(user))
-    .catch(err => res.status(500).json({ Error: err }));
+// Create a new user route
+router.post('/', (req, res) => {
+    const newUser = new User(req.body)
+        newUser.save().then(newUser => {
+            res.status(201).json(newUser);
+        });
 });
-
-// UPDATE A USER
-router.put("/api/users/:id", (req, res) => {
-  User
-  .findByIdAndUpdate(req.params.id, { $set: req.body })
-    .then(user => res.status(204).json(user))
-    .catch(err => res.status(500).json({ Error: err }));
+// // Route responsible for updating an existing user by id
+router.put('/:id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body)
+        .then(updatedUser => {
+            res.status(204).json(updatedUser);
+        });
 });
-
-// DELETE A USER
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  User.findByIdAndRemove(id).then(deletedUser => {
-    res.status(200).json(deletedUser);
-  });
+// // Route responsible for deleting a user
+router.delete('/:id', (req,res) => {
+    User
+        .findByIdAndRemove(req.params.id)
+        .then(deletedUser => {
+            res.status(200).json(deletedUser);
+        });
 });
 
 module.exports = router;
