e32edd50ea973ed07725299ac1e90b601ae82fa3
andrew <andrewjthomsen@yahoo.com>
2019-08-12 18:56:22 -0700

    Rewrote file in attempt to solve routing issue for post, put and get by id routes. Attempt to resolve was successful. Error must have arisen from the way the routes were written.

--- server/Router/blogsRoute.js
+++ server/Router/blogsRoute.js
@@ -1,84 +1,67 @@
 const express = require("express");
 const router = express.Router();
-const Blog = require("../Models/Blog");
-const User = require("../Models/User");
+const Blog = require("../models/Blog"); 
+const User = require("../models/User"); 
 
-// GET ALL BLOGS
+// Responsible for getting all blogs
 router.get("/", (req, res) => {
-  Blog.find().then(blogs => {
-    res.status(200).json(blogs);
-  });
-});
-
-// GET ALL FEATURED BLOGS
-router.get("/featured", (req, res) => {
-  Blog.where({ featured: true })
-    .then(blogs => res.status(200).json(blogs))
-    .catch(err => res.status(500).json({ error: err }));
-});
-
-// GET SINGLE BLOG --> FAILING TESTING
-router.get("/api/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByID(id)
-    .then(blog => {
-      blog
-        ? res.status(200).json(blog)
-        : res.status(404).json({ Error: "Couldn't locate blog with that id!" });
+  Blog.find()
+    .then(Blogs => {
+      res.status(200).json(Blogs);
     })
-    .catch(err => res.status(500).json({ Error: err }));
 });
 
-// CREATE NEW BLOG + associate to userId with .post()
-// * ROUTE NOT WORKING
-router.post("/api/blogs/", (req, res) => {
-  // New Higher scope variable
+// Responsible for getting featured blogs
+router.get("/featured", (req, res) => {
+  Blog.where("featured", true)
+    .then(blogs => {
+      res.status(200).json(blogs);
+    })
+});
+
+// Responsible for getting a single blog 
+router.get("/:id", (req, res) => {
+  Blog.findById(req.params.id)
+    .then(blogs => {
+      if (!blogs) res.status(404).send();
+      res.status(200).json(blogs);
+    })
+    .catch(err => res.status(404));
+});
+
+//Responsible for creating a new blog and associating it to userId
+router.post("/", (req, res) => {
   let dbUser = null;
-  // Fetch user in higher scope variable
-  User
-    .findById(req.body.author)
+  User.findById(req.body.author)
     .then(user => {
-      // store fetched user in the higher scope variable
       dbUser = user;
-
-      // Creation of new blog
       const newBlog = new Blog(req.body);
-
-      // Bind the user
       newBlog.author = user._id;
-      // save to db
       return newBlog.save();
     })
     .then(blog => {
-      // Push blog to array of user's blogs
+      console.log(dbUser);
       dbUser.blogs.push(blog);
-      // Save user to db and respond to http req with copied newly created blog
-      dbUser
-        .save()
-        .then(() => res.status(201).json(blog))
-        .catch(err => res.status(500).json({ Error: err }));
-    });
+      dbUser.save().then(() => res.status(201).json(blog));
+    })
 });
 
-// UPDATE A BLOG
-// *Route isn't firing
-router.put("/api/blogs/:id", (req, res) => {
-  Blog.findByIdAndUpdate(req.params.id, { $set: req.body })
+// Responsible for updating an existing blog
+router.put("/:id", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body)
     .then(blogs => {
+      if (!blogs) return res.status(404).send;
       res.status(204).json(blogs);
     })
-    .catch(e => res.status(500).send("Error, failed to update blog"));
 });
 
-// DELETE A USER
+// Responsible for deleting a blog
 router.delete("/:id", (req, res) => {
-  let id = req.params.id;
-  Blog.findByIdAndRemove(id)
+  Blog.findByIdAndRemove(req.params.id)
     .then(blogs => {
       if (!blogs) return res.status(200).json(blogs);
       res.status(200).json(blogs);
     })
-    .catch(error => res.status(404).send("Error, couldn't delete!"));
 });
 
 module.exports = router;
